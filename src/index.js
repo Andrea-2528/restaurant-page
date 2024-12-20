851892e35d3cc53d8179eb6141deb395cc8c8805
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2024-12-20 18:51:36 +0100

    Webpack setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//CSS import
+import "./styles.css";
+//Images import only if needed in JS, otherwise just link in HTML
+
+//JS import
+//this should also import from existing modules, end of tree
+
+// Run npx webpack serve to start the dev server
+// located at http://localhost:8080
+// kill it with CTRL + C
+
+alert("Hello World");
